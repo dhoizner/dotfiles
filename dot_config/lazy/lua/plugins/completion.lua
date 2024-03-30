93532be882278dd443e2305399e83a6d19dbd1a4
Dan Hoizner <dan.hoizner@gmail.com>
2024-03-29 22:40:32 -0400

    lazyvim changes

--- dot_config/lazy/lua/plugins/completion.lua
+++ dot_config/lazy/lua/plugins/completion.lua
@@ -1,34 +1,5 @@
 return {
   {
-    "telescope.nvim",
-    dependencies = {
-      "ThePrimeagen/harpoon",
-      "joshmedeski/telescope-smart-goto.nvim",
-      "nvim-telescope/telescope-live-grep-args.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      },
-      {
-        "danielfalk/smart-open.nvim",
-        branch = "0.2.x",
-        config = function() end,
-        dependencies = {
-          "kkharji/sqlite.lua",
-          { "nvim-telescope/telescope-fzy-native.nvim" },
-        },
-      },
-    },
-    config = function(_, _)
-      local telescope = require("telescope")
-      telescope.load_extension("harpoon")
-      telescope.load_extension("smart_goto")
-      telescope.load_extension("smart_open")
-      telescope.load_extension("live_grep_args")
-      telescope.load_extension("fzf")
-    end,
-  },
-  {
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
--- dot_config/lazy/lua/plugins/disabled.lua
+++ dot_config/lazy/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   -- disable dashboard
-  { "nvimdev/dashboard-nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
